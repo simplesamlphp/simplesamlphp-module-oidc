72f6921eb8c12f5d8fa7843536fc3ee7ff3cdb43
Sergio Gómez <sergio@uco.es>
2018-04-01 13:32:59 +0200

    [ClientEntity] Added delete action
    Signed-off-by: Sergio Gómez <sergio@uco.es>

--- tests/Controller/ClientControllerTest.php
+++ tests/Controller/ClientControllerTest.php
@@ -16,6 +16,7 @@ use SimpleSAML\Modules\OpenIDConnect\Controller\ClientController;
 use SimpleSAML\Modules\OpenIDConnect\Entity\ClientEntity;
 use SimpleSAML\Modules\OpenIDConnect\Repositories\ClientRepository;
 use SimpleSAML\Modules\OpenIDConnect\Services\DatabaseMigration;
+use SimpleSAML\Modules\OpenIDConnect\Services\SessionMessagesService;
 use SimpleSAML\Modules\OpenIDConnect\Services\TemplateFactory;
 use Zend\Diactoros\ServerRequestFactory;
 
@@ -47,6 +48,7 @@ class ClientControllerTest extends TestCase
     public function tearDown()
     {
         unset($_GET['id']);
+        unset($_POST['secret']);
         $clients = self::$repository->findAll();
 
         foreach ($clients as $client) {
@@ -59,7 +61,7 @@ class ClientControllerTest extends TestCase
         $templateFactory = $this->prophesize(TemplateFactory::class);
         $templateFactory->render('oidc:clients/index.twig', ['clients' => []])->shouldBeCalled();
 
-        $controller = new ClientController(self::$repository, $templateFactory->reveal());
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
         $controller->index(ServerRequestFactory::fromGlobals());
     }
 
@@ -71,7 +73,7 @@ class ClientControllerTest extends TestCase
         $templateFactory = $this->prophesize(TemplateFactory::class);
         $templateFactory->render('oidc:clients/index.twig', ['clients' => [$client]])->shouldBeCalled();
 
-        $controller = new ClientController(self::$repository, $templateFactory->reveal());
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
         $controller->index(ServerRequestFactory::fromGlobals());
     }
 
@@ -85,7 +87,7 @@ class ClientControllerTest extends TestCase
         $templateFactory = $this->prophesize(TemplateFactory::class);
         $templateFactory->render('oidc:clients/show.twig', ['client' => $client])->shouldBeCalled();
 
-        $controller = new ClientController(self::$repository, $templateFactory->reveal());
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
         $controller->show(ServerRequestFactory::fromGlobals());
     }
 
@@ -96,7 +98,7 @@ class ClientControllerTest extends TestCase
     {
         $templateFactory = $this->prophesize(TemplateFactory::class);
 
-        $controller = new ClientController(self::$repository, $templateFactory->reveal());
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
         $controller->show(ServerRequestFactory::fromGlobals());
     }
 
@@ -108,8 +110,95 @@ class ClientControllerTest extends TestCase
         $_GET['id'] = 'client2';
         $templateFactory = $this->prophesize(TemplateFactory::class);
 
-        $controller = new ClientController(self::$repository, $templateFactory->reveal());
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
         $controller->show(ServerRequestFactory::fromGlobals());
+    }
+
+    public function testAskConfirmValidClientDeleteRoute()
+    {
+        $client = self::getClient('client1');
+        self::$repository->add($client);
+
+        $_GET['id'] = 'client1';
+
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+        $templateFactory->render('oidc:clients/delete.twig', ['client' => $client])->shouldBeCalled();
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals());
+    }
+
+    /**
+     * @expectedException \SimpleSAML_Error_BadRequest
+     */
+    public function testErrorWithoutIdDeleteRoute()
+    {
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals());
+    }
+
+    /**
+     * @expectedException \SimpleSAML_Error_NotFound
+     */
+    public function testErrorWithoutValidIdDeleteRoute()
+    {
+        $_GET['id'] = 'client2';
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals());
+    }
+
+    public function testConfirmedValidClientDeleteRoute()
+    {
+        $client = self::getClient('client1');
+        self::$repository->add($client);
+
+        $_GET['id'] = 'client1';
+        $_POST['secret'] = 'clientsecret';
+
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals()->withMethod('post'));
+
+        $foundClient = self::$repository->findById('client1');
+        $this->assertNull($foundClient);
+    }
+
+    /**
+     * @expectedException \SimpleSAML_Error_BadRequest
+     */
+    public function testErrorWithoutSecretConfirmDeleteRoute()
+    {
+        $client = self::getClient('client1');
+        self::$repository->add($client);
+
+        $_GET['id'] = 'client1';
+
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals()->withMethod('post'));
+    }
+
+    /**
+     * @expectedException \SimpleSAML_Error_BadRequest
+     */
+    public function testErrorWithoutValidSecretConfirmDeleteRoute()
+    {
+        $client = self::getClient('client1');
+        self::$repository->add($client);
+
+        $_GET['id'] = 'client1';
+        $_POST['secret'] = 'invalidsecret';
+
+        $templateFactory = $this->prophesize(TemplateFactory::class);
+
+        $controller = new ClientController(self::$repository, $templateFactory->reveal(), new SessionMessagesService());
+        $controller->delete(ServerRequestFactory::fromGlobals()->withMethod('post'));
     }
 
     private static function getClient(string $id)
