7a21a7145c22c4df2e8587f36dd5fe26cee3d3f8
Sergio Gómez <sergio@uco.es>
2018-04-09 12:41:37 +0200

    [Domain] Added authcode
    Signed-off-by: Sergio Gómez <sergio@uco.es>

---
+++ lib/Repositories/AuthCodeRepository.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file is part of the simplesamlphp-module-oidc.
+ *
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SimpleSAML\Modules\OpenIDConnect\Repositories;
+
+use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
+use League\OAuth2\Server\Exception\UniqueTokenIdentifierConstraintViolationException;
+use League\OAuth2\Server\Repositories\AuthCodeRepositoryInterface;
+use SimpleSAML\Modules\OpenIDConnect\Entity\AuthCodeEntity;
+use SimpleSAML\Modules\OpenIDConnect\Utils\TimestampGenerator;
+
+class AuthCodeRepository extends AbstractDatabaseRepository implements AuthCodeRepositoryInterface
+{
+    const TABLE_NAME = 'oidc_auth_code';
+
+    public function getTableName()
+    {
+        return $this->database->applyPrefix(self::TABLE_NAME);
+    }
+
+    public function getNewAuthCode()
+    {
+        return new AuthCodeEntity();
+    }
+
+    public function persistNewAuthCode(AuthCodeEntityInterface $authCodeEntity)
+    {
+        $this->database->write(
+            "INSERT INTO {$this->getTableName()} (id, scopes, expires_at, user_id, client_id, is_revoked, redirect_uri) VALUES (:id, :scopes, :expires_at, :user_id, :client_id, :is_revoked, :redirect_uri)",
+            $authCodeEntity->getState()
+        );
+    }
+
+    /**
+     * Find Access Token by id.
+     *
+     * @param $codeId
+     *
+     * @return null|AuthCodeEntityInterface
+     */
+    public function findById($codeId)
+    {
+        $stmt = $this->database->read(
+            "SELECT * FROM {$this->getTableName()} WHERE id = :id",
+            [
+                'id' => $codeId,
+            ]
+        );
+
+        if (!$rows = $stmt->fetchAll()) {
+            return null;
+        }
+
+        $data = current($rows);
+        $data['client'] = (new ClientRepository())->findById($data['client_id']);
+
+        return AuthCodeEntity::fromState($data);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function revokeAuthCode($codeId)
+    {
+        /** @var AuthCodeEntity $authCode */
+        $authCode = $this->findById($codeId);
+
+        if (!$authCode) {
+            throw new \RuntimeException("AuthCode not found: {$codeId}");
+        }
+
+        $authCode->revoke();
+        $this->update($authCode);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAuthCodeRevoked($tokenId)
+    {
+        /** @var AuthCodeEntity $authCode */
+        $authCode = $this->findById($tokenId);
+
+        if (!$authCode) {
+            throw new \RuntimeException("AuthCode not found: {$tokenId}");
+        }
+
+        return $authCode->isRevoked();
+    }
+
+    /**
+     * Removes expired auth codes.
+     */
+    public function removeExpired()
+    {
+        $this->database->write(
+            "DELETE FROM {$this->getTableName()} WHERE expires_at < :now",
+            [
+                'now' => TimestampGenerator::utc()->format('Y-m-d H:i:s'),
+            ]
+        );
+    }
+
+    private function update(AuthCodeEntity $authCodeEntity)
+    {
+        $this->database->write(
+            "UPDATE {$this->getTableName()} SET scopes = :scopes, expires_at = :expires_at, user_id = :user_id, client_id = :client_id, is_revoked = :is_revoked, redirect_uri = :redirect_uri WHERE id = :id",
+            $authCodeEntity->getState()
+        );
+    }
+
+}
---
+++ tests/Repositories/AuthCodeRepositoryTest.php
@@ -0,0 +1,122 @@
+<?php
+/*
+ * This file is part of the simplesamlphp-module-oidc.
+ *
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\SimpleSAML\Modules\OpenIDConnect\Repositories;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Modules\OpenIDConnect\Entity\ScopeEntity;
+use SimpleSAML\Modules\OpenIDConnect\Entity\UserEntity;
+use SimpleSAML\Modules\OpenIDConnect\Repositories\AuthCodeRepository;
+use SimpleSAML\Modules\OpenIDConnect\Repositories\ClientRepository;
+use SimpleSAML\Modules\OpenIDConnect\Repositories\UserRepository;
+use SimpleSAML\Modules\OpenIDConnect\Services\DatabaseMigration;
+
+class AuthCodeRepositoryTest extends TestCase
+{
+    const CLIENT_ID = 'auth_code_client_id';
+    const USER_ID = 'auth_code_user_id';
+    const AUTH_CODE_ID = 'auth_code_id';
+    const REDIRECT_URI = 'http://localhost/redirect';
+
+    /**
+     * @var AuthCodeRepository
+     */
+    protected static $repository;
+
+    public static function setUpBeforeClass()
+    {
+        $config = [
+            'database.dsn' => 'sqlite::memory:',
+            'database.username' => null,
+            'database.password' => null,
+            'database.prefix' => 'phpunit_',
+            'database.persistent' => true,
+            'database.slaves' => [],
+        ];
+
+        \SimpleSAML_Configuration::loadFromArray($config, '', 'simplesaml');
+        (new DatabaseMigration())->migrate();
+
+        $client = ClientRepositoryTest::getClient(self::CLIENT_ID);
+        (new ClientRepository())->add($client);
+        $user = UserEntity::fromData(self::USER_ID);
+        (new UserRepository())->add($user);
+
+        self::$repository = new AuthCodeRepository();
+    }
+
+    public function testGetTableName()
+    {
+        $this->assertSame('phpunit_oidc_auth_code', self::$repository->getTableName());
+    }
+
+    public function testAddAndFound()
+    {
+        $scopes = [
+            ScopeEntity::fromData('openid'),
+        ];
+
+        $authCode = self::$repository->getNewAuthCode();
+
+        $authCode->setIdentifier(self::AUTH_CODE_ID);
+        $authCode->setClient(ClientRepositoryTest::getClient(self::CLIENT_ID));
+        $authCode->setUserIdentifier(self::USER_ID);
+        $authCode->setExpiryDateTime(new \DateTime('yesterday'));
+        $authCode->setRedirectUri(self::REDIRECT_URI);
+        foreach ($scopes as $scope) {
+            $authCode->addScope($scope);
+        }
+
+        self::$repository->persistNewAuthCode($authCode);
+
+        $foundAuthCode = self::$repository->findById(self::AUTH_CODE_ID);
+
+        $this->assertEquals($authCode, $foundAuthCode);
+    }
+
+    public function testAddAndNotFound()
+    {
+        $notFoundAuthCode = self::$repository->findById('nocode');
+
+        $this->assertNull($notFoundAuthCode);
+    }
+
+    public function testRevokeCode()
+    {
+        self::$repository->revokeAuthCode(self::AUTH_CODE_ID);
+        $isRevoked = self::$repository->isAuthCodeRevoked(self::AUTH_CODE_ID);
+
+        $this->assertTrue($isRevoked);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testErrorRevokeInvalidAuthCode()
+    {
+        self::$repository->revokeAuthCode('nocode');
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testErrorCheckIsRevokedInvalidAuthCode()
+    {
+        self::$repository->isAuthCodeRevoked('nocode');
+    }
+
+    public function testRemoveExpired()
+    {
+        self::$repository->removeExpired();
+        $notFoundAuthCode = self::$repository->findById(self::AUTH_CODE_ID);
+
+        $this->assertNull($notFoundAuthCode);
+    }
+}
