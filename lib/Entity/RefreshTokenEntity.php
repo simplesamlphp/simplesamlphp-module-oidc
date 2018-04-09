f5c7660bef236841cd733b1125888ba1a7c411d8
Sergio Gómez <sergio@uco.es>
2018-04-09 15:54:11 +0200

    [Domain] Added refresh token
    Signed-off-by: Sergio Gómez <sergio@uco.es>

---
+++ lib/Entity/RefreshTokenEntity.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the simplesamlphp-module-oidc.
+ *
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SimpleSAML\Modules\OpenIDConnect\Entity;
+
+use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+use League\OAuth2\Server\Entities\Traits\RefreshTokenTrait;
+use SimpleSAML\Modules\OpenIDConnect\Entity\Interfaces\MementoInterface;
+use SimpleSAML\Modules\OpenIDConnect\Entity\Traits\RevokeTokenTrait;
+use SimpleSAML\Modules\OpenIDConnect\Utils\TimestampGenerator;
+
+class RefreshTokenEntity implements RefreshTokenEntityInterface, MementoInterface
+{
+    use RefreshTokenTrait, EntityTrait, RevokeTokenTrait;
+
+    public static function fromState(array $state)
+    {
+        $refreshToken = new self();
+
+        $refreshToken->identifier = $state['id'];
+        $refreshToken->expiryDateTime = TimestampGenerator::utc($state['expires_at']);
+        $refreshToken->accessToken = $state['access_token'];
+        $refreshToken->isRevoked = $state['is_revoked'];
+
+        return $refreshToken;
+    }
+
+    public function getState(): array
+    {
+        return [
+            'id' => $this->identifier,
+            'expires_at' => $this->expiryDateTime->format('Y-m-d H:i:s'),
+            'access_token_id' => $this->accessToken->getIdentifier(),
+            'is_revoked' => $this->isRevoked,
+        ];
+    }
+}
---
+++ lib/Repositories/RefreshTokenRepository.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the simplesamlphp-module-oidc.
+ *
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SimpleSAML\Modules\OpenIDConnect\Repositories;
+
+use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
+use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
+use SimpleSAML\Modules\OpenIDConnect\Entity\RefreshTokenEntity;
+use SimpleSAML\Modules\OpenIDConnect\Utils\TimestampGenerator;
+
+class RefreshTokenRepository extends AbstractDatabaseRepository implements RefreshTokenRepositoryInterface
+{
+    const TABLE_NAME = 'oidc_refresh_token';
+
+    public function getTableName()
+    {
+        return $this->database->applyPrefix(self::TABLE_NAME);
+    }
+
+    public function getNewRefreshToken()
+    {
+        return new RefreshTokenEntity();
+    }
+
+    /**
+     * @param RefreshTokenEntityInterface|RefreshTokenEntity $refreshTokenEntity
+     */
+    public function persistNewRefreshToken(RefreshTokenEntityInterface $refreshTokenEntity)
+    {
+        $this->database->write(
+            "INSERT INTO {$this->getTableName()} (id, expires_at, access_token_id, is_revoked) VALUES (:id, :expires_at, :access_token_id, :is_revoked)",
+            $refreshTokenEntity->getState()
+        );
+    }
+
+    /**
+     * Find Refresh Token by id.
+     *
+     * @param $tokenId
+     *
+     * @return null|RefreshTokenEntity
+     */
+    public function findById($tokenId)
+    {
+        $stmt = $this->database->read(
+            "SELECT * FROM {$this->getTableName()} WHERE id = :id",
+            [
+                'id' => $tokenId,
+            ]
+        );
+
+        if (!$rows = $stmt->fetchAll()) {
+            return null;
+        }
+
+        $data = current($rows);
+        $data['access_token'] = (new AccessTokenRepository())->findById($data['access_token_id']);
+
+        return RefreshTokenEntity::fromState($data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function revokeRefreshToken($tokenId)
+    {
+        $refreshToken = $this->findById($tokenId);
+
+        if (!$refreshToken) {
+            throw new \RuntimeException("RefreshToken not found: {$tokenId}");
+        }
+
+        $refreshToken->revoke();
+        $this->update($refreshToken);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isRefreshTokenRevoked($tokenId)
+    {
+        $refreshToken = $this->findById($tokenId);
+
+        if (!$refreshToken) {
+            throw new \RuntimeException("RefreshToken not found: {$tokenId}");
+        }
+
+        return $refreshToken->isRevoked();
+    }
+
+    /**
+     * Removes expired refresh tokens.
+     */
+    public function removeExpired()
+    {
+        $this->database->write(
+            "DELETE FROM {$this->getTableName()} WHERE expires_at < :now",
+            [
+                'now' => TimestampGenerator::utc()->format('Y-m-d H:i:s'),
+            ]
+        );
+    }
+
+    private function update(RefreshTokenEntity $refreshTokenEntity)
+    {
+        $this->database->write(
+            "UPDATE {$this->getTableName()} SET expires_at = :expires_at, access_token_id = :access_token_id, is_revoked = :is_revoked WHERE id = :id",
+            $refreshTokenEntity->getState()
+        );
+    }
+}
