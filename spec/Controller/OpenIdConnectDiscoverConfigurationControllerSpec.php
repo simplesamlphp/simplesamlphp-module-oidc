3eb32fac91a454c56f3d7e9f74ef4bae0a9464a5
Marko Ivančić <marko.ivancic@srce.hr>
2020-09-11 11:00:42 +0200

    Adjust spec for oidc configuration

--- spec/Controller/OpenIdConnectDiscoverConfigurationControllerSpec.php
+++ spec/Controller/OpenIdConnectDiscoverConfigurationControllerSpec.php
@@ -69,7 +69,7 @@ class OpenIdConnectDiscoverConfigurationControllerSpec extends ObjectBehavior
             'userinfo_endpoint' => 'http://localhost/userinfo.php',
             'jwks_uri' => 'http://localhost/jwks.php',
             'scopes_supported' => ['openid'],
-            'response_types_supported' => ['code', 'token', 'id_token token'],
+            'response_types_supported' => ['code', 'token'],
             'subject_types_supported' => ['public'],
             'id_token_signing_alg_values_supported' => ['RS256'],
             'code_challenge_methods_supported' => ['plain', 'S256'],
