c0c883f9e4c2fc1cbef426dc617af6f2ff5b4eaf
Marko Ivancic <cicnavi@gmail.com>
2020-09-30 14:51:27 +0200

    Generate 'kid' parameter based on public certificate fingerprint (#46)
    * Set default value when trying to get pkce configuration option
    
    * Generate 'kid' parameter based on public certificate fingerprint
    
    * Remove pkce config check and adjust spec
    
    * Fix psalm and phpcs errors
    
    * Remove unsupported response type
    
    * Adjust spec for oidc configuration
    
    Co-authored-by: Marko Ivančić <marko.ivancic@srce.hr>

---
+++ lib/Utils/FingerprintGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SimpleSAML\Modules\OpenIDConnect\Utils;
+
+class FingerprintGenerator
+{
+    /**
+     * Generate a fingerprint (hash) for a provided file.
+     *
+     * @param string $path Location of file
+     * @param string $algo One of the supported algorithms (see hash_algos() function)
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function forFile($path, $algo = 'md5')
+    {
+        $fingerprint = hash_file($algo, $path);
+
+        if (false === (bool) $fingerprint) {
+            throw new \InvalidArgumentException('Could not create a fingerprint for provided file using' .
+                ' provided algorithm.');
+        }
+
+        return $fingerprint;
+    }
+
+    /**
+     * Generate a fingerprint (hash) for a provided string.
+     *
+     * @param string $content Content for which to create a fingerprint
+     * @param string $algo One of the supported algorithms (see hash_algos() function)
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function forString($content, $algo = 'md5')
+    {
+        $fingerprint = hash($algo, $content);
+
+        if (false === (bool) $fingerprint) {
+            throw new \InvalidArgumentException('Could not create a fingerprint for provided content using' .
+                ' provided algorithm.');
+        }
+
+        return $fingerprint;
+    }
+}
