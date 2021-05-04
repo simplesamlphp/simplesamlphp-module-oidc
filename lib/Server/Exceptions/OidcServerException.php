9cd8a3070b8e1c26bc296db74c7865fdfc1b5ed5
Marko Ivančić <marko.ivancic@srce.hr>
2021-05-04 10:50:03 +0200

    Add unsupported_response_type error exception

--- lib/Server/Exceptions/OidcServerException.php
+++ lib/Server/Exceptions/OidcServerException.php
@@ -6,5 +6,17 @@ use League\OAuth2\Server\Exception\OAuthServerException;
 
 class OidcServerException extends OAuthServerException
 {
+    /**
+     * Unsupported response type error.
+     *
+     * @param null $redirectUri
+     * @return self
+     */
+    public static function unsupportedResponseType($redirectUri = null): OidcServerException
+    {
+        $errorMessage = 'The response type is not supported by the authorization server.';
+        $hint = 'Check that all required parameters have been provided';
 
+        return new self($errorMessage, 2, 'unsupported_response_type', 400, $hint, $redirectUri);
+    }
 }
