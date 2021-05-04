5c41ae3fc9c120e71386b9e0287a8d871c23d886
Marko Ivančić <marko.ivancic@srce.hr>
2021-05-04 16:01:18 +0200

    Return unsupported_response_type error for invalid response types
    This is a OIDC conformance reimplementation of exception handling
    in which a proper error message is returned to OIDC client
    if response type is invalid (error unsupported_response_type is
    returned to the OIDC client using HTTP redirect response).
    This could be replaced by simply showing an error page (according
    to the conformance test), so this could be done much simpler
    (less need for overriding league/oauth2 package).

--- lib/Server/AuthorizationServer.php
+++ lib/Server/AuthorizationServer.php
@@ -2,30 +2,73 @@
 
 namespace SimpleSAML\Modules\OpenIDConnect\Server;
 
+use Defuse\Crypto\Key;
 use League\OAuth2\Server\AuthorizationServer as OAuth2AuthorizationServer;
+use League\OAuth2\Server\CryptKey;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+use League\OAuth2\Server\RequestEvent;
 use League\OAuth2\Server\RequestTypes\AuthorizationRequest;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use SimpleSAML\Modules\OpenIDConnect\Server\Exceptions\OidcServerException;
 
 class AuthorizationServer extends OAuth2AuthorizationServer
 {
     /**
+     * @var ClientRepositoryInterface
+     */
+    protected $clientRepository;
+
+    /**
+     * New server instance.
+     *
+     * @param ClientRepositoryInterface $clientRepository
+     * @param AccessTokenRepositoryInterface $accessTokenRepository
+     * @param ScopeRepositoryInterface $scopeRepository
+     * @param CryptKey|string $privateKey
+     * @param string|Key $encryptionKey
+     * @param null|ResponseTypeInterface $responseType
+     */
+    public function __construct(
+        ClientRepositoryInterface $clientRepository,
+        AccessTokenRepositoryInterface $accessTokenRepository,
+        ScopeRepositoryInterface $scopeRepository,
+        $privateKey,
+        $encryptionKey,
+        ResponseTypeInterface $responseType = null
+    ) {
+        parent::__construct(
+            $clientRepository,
+            $accessTokenRepository,
+            $scopeRepository,
+            $privateKey,
+            $encryptionKey,
+            $responseType
+        );
+
+        $this->clientRepository = $clientRepository;
+    }
+
+    /**
      * Validate an authorization request
      *
      * @param ServerRequestInterface $request
      *
+     * @return AuthorizationRequest
      * @throws OAuthServerException
      *
-     * @return AuthorizationRequest
      */
     public function validateAuthorizationRequest(ServerRequestInterface $request): AuthorizationRequest
     {
-        // TODO mivanci
-        // Check client_id
-        // Check redirect_uri
-        // Get state
-        // Check if response_type is enabled
+        // TODO mivanci Since client and redirect uri validation is now in this class, we should also implement
+        // custom grants and override validation methods in each grant...
+        $client = $this->getClientOrFail($request);
+        $redirectUri = $this->getRedirectUriOrFail($client, $request);
+        $state = $request->getQueryParams()['state'] ?? null;
 
         foreach ($this->enabledGrantTypes as $grantType) {
             if ($grantType->canRespondToAuthorizationRequest($request)) {
@@ -33,6 +76,82 @@ class AuthorizationServer extends OAuth2AuthorizationServer
             }
         }
 
-        throw OidcServerException::unsupportedResponseType();
+        $payload = [];
+        if ($state !== null) {
+            $payload['state'] = $state;
+        }
+        // Client and redirect URI validation passed, so we can safely redirect to the RP.
+        throw OidcServerException::unsupportedResponseType($redirectUri, $payload);
+    }
+
+    /**
+     * Get client entity.
+     * @param ServerRequestInterface $request
+     * @return ClientEntityInterface
+     * @throws OidcServerException If client_id is missing or value is invalid.
+     */
+    protected function getClientOrFail(ServerRequestInterface $request): ClientEntityInterface
+    {
+        $clientId = $request->getQueryParams()['client_id'] ?? $request->getServerParams()['PHP_AUTH_USER'] ?? null;
+
+        if ($clientId === null) {
+            throw OidcServerException::invalidRequest('client_id');
+        }
+
+        $client = $this->clientRepository->getClientEntity($clientId);
+
+        if ($client instanceof ClientEntityInterface === false) {
+            $this->getEmitter()->emit(new RequestEvent(RequestEvent::CLIENT_AUTHENTICATION_FAILED, $request));
+            throw OidcServerException::invalidClient($request);
+        }
+
+        return $client;
+    }
+
+    /**
+     * Get redirect URI.
+     * @param ClientEntityInterface $client
+     * @param ServerRequestInterface $request
+     * @return string
+     * @throws OidcServerException
+     */
+    private function getRedirectUriOrFail(ClientEntityInterface $client, ServerRequestInterface $request): string
+    {
+        $redirectUri = $request->getQueryParams()['redirect_uri'] ?? null;
+
+        // Return default redirect URI if none provided on request.
+        if ($redirectUri === null) {
+            // In OAuth2, redirect_uri is optional if only one is registered.
+            /** @psalm-suppress PossiblyInvalidArgument */
+            if (
+                empty($client->getRedirectUri()) ||
+                (\is_array($client->getRedirectUri()) && \count($client->getRedirectUri()) !== 1)
+            ) {
+                $this->getEmitter()->emit(new RequestEvent(RequestEvent::CLIENT_AUTHENTICATION_FAILED, $request));
+                throw OidcServerException::invalidClient($request);
+            }
+
+            /** @psalm-suppress InvalidReturnStatement */
+            return \is_array($client->getRedirectUri())
+                ? $client->getRedirectUri()[0]
+                : $client->getRedirectUri();
+        }
+
+        /** @psalm-suppress PossiblyInvalidArgument */
+        if (
+            \is_string($client->getRedirectUri()) &&
+            (\strcmp($client->getRedirectUri(), $redirectUri) !== 0)
+        ) {
+            $this->getEmitter()->emit(new RequestEvent(RequestEvent::CLIENT_AUTHENTICATION_FAILED, $request));
+            throw OidcServerException::invalidClient($request);
+        } elseif (
+            \is_array($client->getRedirectUri()) &&
+            \in_array($redirectUri, $client->getRedirectUri(), true) === false
+        ) {
+            $this->getEmitter()->emit(new RequestEvent(RequestEvent::CLIENT_AUTHENTICATION_FAILED, $request));
+            throw OidcServerException::invalidClient($request);
+        }
+
+        return $redirectUri;
     }
 }
--- lib/Server/Exceptions/OidcServerException.php
+++ lib/Server/Exceptions/OidcServerException.php
@@ -3,20 +3,79 @@
 namespace SimpleSAML\Modules\OpenIDConnect\Server\Exceptions;
 
 use League\OAuth2\Server\Exception\OAuthServerException;
+use Throwable;
 
 class OidcServerException extends OAuthServerException
 {
     /**
+     * @var array
+     */
+    protected $payload;
+
+    /**
+     * Throw a new exception.
+     *
+     * @param string      $message        Error message
+     * @param int         $code           Error code
+     * @param string      $errorType      Error type
+     * @param int         $httpStatusCode HTTP status code to send (default = 400)
+     * @param null|string $hint           A helper hint
+     * @param null|string $redirectUri    A HTTP URI to redirect the user back to
+     * @param Throwable|null   $previous       Previous exception
+     */
+    public function __construct(
+        $message,
+        $code,
+        $errorType,
+        $httpStatusCode = 400,
+        $hint = null,
+        $redirectUri = null,
+        Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $errorType, $httpStatusCode, $hint, $redirectUri, $previous);
+        $this->payload = [
+            'error'             => $errorType,
+            'error_description' => $message,
+        ];
+    }
+
+    /**
      * Unsupported response type error.
      *
-     * @param null $redirectUri
+     * @param string|null $redirectUri
+     * @param array $additionalPayload
      * @return self
      */
-    public static function unsupportedResponseType($redirectUri = null): OidcServerException
-    {
+    public static function unsupportedResponseType(
+        string $redirectUri = null,
+        array $additionalPayload = []
+    ): OidcServerException {
         $errorMessage = 'The response type is not supported by the authorization server.';
         $hint = 'Check that all required parameters have been provided';
 
-        return new self($errorMessage, 2, 'unsupported_response_type', 400, $hint, $redirectUri);
+        $exception = new self($errorMessage, 2, 'unsupported_response_type', 400, $hint, $redirectUri);
+        $exception->setPayload(\array_merge($exception->getPayload(), $additionalPayload));
+
+        return $exception;
+    }
+
+    /**
+     * Updates the current payload.
+     *
+     * @param array $payload
+     */
+    public function setPayload(array $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Returns the current payload.
+     *
+     * @return array
+     */
+    public function getPayload(): array
+    {
+        return $this->payload;
     }
 }
