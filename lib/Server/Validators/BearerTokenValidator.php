6489f68f4821e7030be43dfcd81ba9740300235d
Marko Ivančić <marko.ivancic@srce.hr>
2021-08-20 09:08:49 +0200

    WIP BearerTokenValidator

---
+++ lib/Server/Validators/BearerTokenValidator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SimpleSAML\Module\oidc\Server\Validators;
+
+use League\OAuth2\Server\AuthorizationValidators\BearerTokenValidator as OAuth2BearerTokenValidator;
+
+class BearerTokenValidator extends OAuth2BearerTokenValidator
+{
+
+}
