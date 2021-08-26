cd1deab7c357c50ac8eaf6e599956f561d0fe55a
Patrick Radtke <patrick@cirrusidentity.com>
2021-08-26 14:47:22 -0700

    Fix conformance.sql; add support for requested claims to .well-known

--- lib/Services/OidcOpenIdProviderMetadataService.php
+++ lib/Services/OidcOpenIdProviderMetadataService.php
@@ -47,6 +47,7 @@ class OidcOpenIdProviderMetadataService
         $this->metadata['token_endpoint_auth_methods_supported'] = ['client_secret_post', 'client_secret_basic'];
         $this->metadata['request_parameter_supported'] = false;
         $this->metadata['grant_types_supported'] = ['authorization_code', 'refresh_token'];
+        $this->metadata['claims_parameter_supported'] = true;
     }
 
     /**
