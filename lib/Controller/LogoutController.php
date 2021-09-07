7a9a65fbd4f3f95ff9a2f5eb6fe140d953a100b9
Marko Ivančić <marko.ivancic@srce.hr>
2021-09-07 13:52:09 +0200

    Mark TODO

---
+++ lib/Controller/LogoutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SimpleSAML\Module\oidc\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use SimpleSAML\Module\oidc\Server\AuthorizationServer;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
+
+class LogoutController
+{
+    /**
+     * @var AuthorizationServer
+     */
+    private $authorizationServer;
+
+    public function __construct(AuthorizationServer $authorizationServer)
+    {
+        $this->authorizationServer = $authorizationServer;
+    }
+
+    public function __invoke(ServerRequest $request): ResponseInterface
+    {
+        // TODO mivanci logout
+        // * implement RP-Initiated Logout: https://openid.net/specs/openid-connect-rpinitiated-1_0.html
+        // * implement Back-Channel Logout: https://openid.net/specs/openid-connect-backchannel-1_0.html
+        // * consider implementing  Front-Channel Logout: https://openid.net/specs/openid-connect-frontchannel-1_0.html
+        //   (FCL has challenges with User Agents Blocking Access to Third-Party Content:
+        //   https://openid.net/specs/openid-connect-frontchannel-1_0.html#ThirdPartyContent)
+
+//        return $this->authorizationServer->respondToLogoutRequest($request, new Response());
+        return new Response(); // ...to satisfy return type, remove when logout handler is implemented.
+    }
+}
