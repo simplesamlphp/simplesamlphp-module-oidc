f86e7b767b5edca992f5ca81c5660f80817f7a62
Marko Ivančić <marko.ivancic@srce.hr>
2021-09-08 14:46:54 +0200

    Update TODO

--- lib/Controller/LogoutController.php
+++ lib/Controller/LogoutController.php
@@ -24,17 +24,37 @@ class LogoutController
     public function __invoke(ServerRequest $request): ResponseInterface
     {
         // TODO mivanci logout
-        // * implement RP-Initiated Logout: https://openid.net/specs/openid-connect-rpinitiated-1_0.html
-        // * implement Back-Channel Logout: https://openid.net/specs/openid-connect-backchannel-1_0.html
-        // * consider implementing  Front-Channel Logout: https://openid.net/specs/openid-connect-frontchannel-1_0.html
-        //   (FCL has challenges with User Agents Blocking Access to Third-Party Content:
-        //   https://openid.net/specs/openid-connect-frontchannel-1_0.html#ThirdPartyContent)
+        // [] RP-Initiated Logout: https://openid.net/specs/openid-connect-rpinitiated-1_0.html
+        //      [] register logout handler during authn. Wait for acr_values branch merge, it introduces authsourceid
+        //          See: \SimpleSAML\Session::registerLogoutHandler, modules/memcookie/www/auth.php:87
+        //          $session = \SimpleSAML\Session::getSessionFromRequest();
+        //          $session->registerLogoutHandler($sourceId, 'class', 'method');
+        //      [] implement 'sid' claim in ID Token during authn.
+        //      [] also note support in discovery
+        //      [] store OP -> RP associations, probably using Session
+        //      [x] add end_session_endpoint to discovery
+        //      [] RP logout request, must support GET and POST:
+        //          [] id_token_hint - optional, recommended
+        //              [] ID token, use it to extract sub, aud, sid...
+        //              [] check issuer, aud, (sub?)...
+        //              [] check that sid is current or recently existed
+        //          [] post_logout_redirect_uri - optional
+        //              [] must be https for public clients, optionally http for confidential
+        //              [] enable registration in client management UI
+        //              [] only allow registered redirect, must be supplied together with id_token_hint
+        //              [] redirect to RP after logout
+        //          [] state - optional
+        //              [] return with redirect to redirect_uri
+        //          [] ui_locales - optional
+        //          [] preferred language for user UI, for example to ask user to allow logout - probably won't handle
 
-        // register logout handler during authn
-//        $session = \SimpleSAML\Session::getSessionFromRequest();
-//        $session->registerLogoutHandler($sourceId, 'class', 'method');
+        // [] implement Back-Channel Logout: https://openid.net/specs/openid-connect-backchannel-1_0.html
+        //      []
 
+        // [] consider implementing  Front-Channel Logout: https://openid.net/specs/openid-connect-frontchannel-1_0.html
+        //      (FCL has challenges with User Agents Blocking Access to Third-Party Content:
+        //      https://openid.net/specs/openid-connect-frontchannel-1_0.html#ThirdPartyContent)
 //        return $this->authorizationServer->respondToLogoutRequest($request, new Response());
-        return new Response(); // ...to satisfy return type, remove when logout handler is implemented.
+        return new Response(); // ...to satisfy return type, adjust when logout handler is implemented.
     }
 }
