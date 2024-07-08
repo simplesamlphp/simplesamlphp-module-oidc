003bb37eaa040e33df29d3324c38e8fbf977633c
Ioannis Igoumenos <ioigoume@gmail.com>
2024-07-08 17:49:04 +0300

    Use PDO::PARAM_BOOL when binding boolean values. Do not assume boolean value to be a tinyint.

--- src/Entities/RefreshTokenEntity.php
+++ src/Entities/RefreshTokenEntity.php
@@ -19,7 +19,6 @@ namespace SimpleSAML\Module\oidc\Entities;
 use DateTimeImmutable;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\RefreshTokenTrait;
-use SimpleSAML\Database;
 use SimpleSAML\Module\oidc\Entities\Interfaces\AccessTokenEntityInterface;
 use SimpleSAML\Module\oidc\Entities\Interfaces\RefreshTokenEntityInterface;
 use SimpleSAML\Module\oidc\Entities\Traits\AssociateWithAuthCodeTrait;
@@ -63,18 +62,11 @@ class RefreshTokenEntity implements RefreshTokenEntityInterface
 
     public function getState(): array
     {
-        $database = Database::getInstance();
-
-        $revoked = $this->isRevoked() ? 'true' : 'false';
-        if($database->getDriver() === 'mysql') {
-            $revoked = (int) $this->isRevoked();
-        }
-
         return [
             'id' => $this->getIdentifier(),
             'expires_at' => $this->getExpiryDateTime()->format('Y-m-d H:i:s'),
             'access_token_id' => $this->getAccessToken()->getIdentifier(),
-            'is_revoked' => $revoked,
+            'is_revoked' => [$this->isRevoked(), PDO::PARAM_BOOL],
             'auth_code_id' => $this->getAuthCodeId(),
         ];
     }
