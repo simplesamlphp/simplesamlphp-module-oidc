9123922e21f0241ff06ba857a6870dde4e331860
Ioannis Igoumenos <ioigoume@gmail.com>
2024-07-09 17:57:44 +0300

    revokeByAuthCodeId: bind using PDO::PARAM_BOOL

--- src/Repositories/Traits/RevokeTokenByAuthCodeIdTrait.php
+++ src/Repositories/Traits/RevokeTokenByAuthCodeIdTrait.php
@@ -4,18 +4,23 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Module\oidc\Repositories\Traits;
 
+use PDO;
+
 trait RevokeTokenByAuthCodeIdTrait
 {
     public function revokeByAuthCodeId(string $authCodeId): void
     {
         $stmt = sprintf(
-            "UPDATE %s SET is_revoked = 1 WHERE auth_code_id = :auth_code_id",
+            "UPDATE %s SET is_revoked = :is_revoked WHERE auth_code_id = :auth_code_id",
             $this->getTableName(),
         );
 
         $this->database->write(
             $stmt,
-            ['auth_code_id' => $authCodeId],
+            [
+                'auth_code_id' => $authCodeId,
+                'is_revoked' => [true, PDO::PARAM_BOOL],
+            ],
         );
     }
 }
