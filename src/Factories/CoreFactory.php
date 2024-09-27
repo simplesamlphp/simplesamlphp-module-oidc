c875ff433bfc9e7aa73fe024c31f3d2e2d54d3dd
Marko Ivancic <cicnavi@gmail.com>
2024-09-27 10:41:47 +0200

    Add CoreFactory with predefined algos (#253)
    Co-authored-by: Marko Ivančić <marko.ivancic@srce.hr>

---
+++ src/Factories/CoreFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\oidc\Factories;
+
+use SimpleSAML\Module\oidc\ModuleConfig;
+use SimpleSAML\Module\oidc\Services\LoggerService;
+use SimpleSAML\OpenID\Algorithms\SignatureAlgorithmBag;
+use SimpleSAML\OpenID\Algorithms\SignatureAlgorithmEnum;
+use SimpleSAML\OpenID\Core;
+use SimpleSAML\OpenID\SupportedAlgorithms;
+
+class CoreFactory
+{
+    public function __construct(
+        protected readonly ModuleConfig $moduleConfig,
+        protected readonly LoggerService $loggerService,
+    ) {
+    }
+
+    /**
+     * @throws \ReflectionException
+     * @throws \SimpleSAML\Error\ConfigurationError
+     */
+    public function build(): Core
+    {
+        $supportedAlgorithms = new SupportedAlgorithms(
+            new SignatureAlgorithmBag(
+                SignatureAlgorithmEnum::from($this->moduleConfig->getFederationSigner()->algorithmId()),
+                SignatureAlgorithmEnum::RS384,
+                SignatureAlgorithmEnum::RS512,
+                SignatureAlgorithmEnum::ES256,
+                SignatureAlgorithmEnum::ES384,
+                SignatureAlgorithmEnum::ES512,
+            ),
+        );
+
+        return new Core(
+            supportedAlgorithms: $supportedAlgorithms,
+            logger: $this->loggerService,
+        );
+    }
+}
