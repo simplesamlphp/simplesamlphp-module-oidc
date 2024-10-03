50cbd3f8050979cf45f557796cd2b58cc9fe9f91
Marko Ivančić <marko.ivancic@srce.hr>
2024-10-03 11:25:08 +0200

    WIP AccessTokenEntityFactory

--- tests/integration/src/Repositories/Traits/RevokeTokenByAuthCodeIdTraitTest.php
+++ tests/integration/src/Repositories/Traits/RevokeTokenByAuthCodeIdTraitTest.php
@@ -6,18 +6,21 @@ namespace SimpleSAML\Test\Module\oidc\integration\Repositories\Traits;
 
 use DateTimeImmutable;
 use DateTimeZone;
+use League\OAuth2\Server\CryptKey;
 use PDO;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use SimpleSAML\Configuration;
 use SimpleSAML\Database;
+use SimpleSAML\Module\oidc\Entities\AccessTokenEntity;
 use SimpleSAML\Module\oidc\Entities\ClientEntity;
 use SimpleSAML\Module\oidc\Entities\Interfaces\ClientEntityInterface;
 use SimpleSAML\Module\oidc\Entities\ScopeEntity;
 use SimpleSAML\Module\oidc\Entities\UserEntity;
 use SimpleSAML\Module\oidc\Factories\Entities\AccessTokenEntityFactory;
 use SimpleSAML\Module\oidc\Factories\Entities\ClientEntityFactory;
+use SimpleSAML\Module\oidc\Helpers;
 use SimpleSAML\Module\oidc\ModuleConfig;
 use SimpleSAML\Module\oidc\Repositories\AbstractDatabaseRepository;
 use SimpleSAML\Module\oidc\Repositories\AccessTokenRepository;
@@ -25,6 +28,7 @@ use SimpleSAML\Module\oidc\Repositories\ClientRepository;
 use SimpleSAML\Module\oidc\Repositories\Traits\RevokeTokenByAuthCodeIdTrait;
 use SimpleSAML\Module\oidc\Repositories\UserRepository;
 use SimpleSAML\Module\oidc\Services\DatabaseMigration;
+use SimpleSAML\Module\oidc\Services\JsonWebTokenBuilderService;
 use Testcontainers\Container\MySQLContainer;
 use Testcontainers\Container\PostgresContainer;
 use Testcontainers\Wait\WaitForHealthCheck;
@@ -53,15 +57,7 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
     public static array $sqliteConfig;
 
     protected AbstractDatabaseRepository $mock;
-
-    /**
-     * @var \SimpleSAML\Module\oidc\Entities\ScopeEntity
-     */
     protected ScopeEntity $scopeEntityOpenId;
-
-    /**
-     * @var \SimpleSAML\Module\oidc\Entities\ScopeEntity
-     */
     protected ScopeEntity $scopeEntityProfile;
 
     private static ?string $containerAddress = null;
@@ -69,6 +65,10 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
     private static ?string $postgresPort = null;
 
     protected MockObject $accessTokenEntityFactoryMock;
+    protected MockObject $accessTokenEntityMock;
+    protected array $accessTokenState;
+    protected AccessTokenEntityFactory $accessTokenEntityFactory;
+    protected CryptKey $privateKey;
 
     public static function setUpBeforeClass(): void
     {
@@ -89,6 +89,42 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
         self::$pgConfig = self::loadPGDatabase();
         self::$mysqlConfig = self::loadMySqlDatabase();
         self::$sqliteConfig = self::loadSqliteDatabase();
+    }
+
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function setUp(): void
+    {
+        $this->scopeEntityOpenId = $this->createStub(ScopeEntity::class);
+        $this->scopeEntityOpenId->method('getIdentifier')->willReturn('openid');
+        $this->scopeEntityOpenId->method('jsonSerialize')->willReturn('openid');
+        $this->scopeEntityProfile = $this->createStub(ScopeEntity::class);
+        $this->scopeEntityProfile->method('getIdentifier')->willReturn('profile');
+        $this->scopeEntityProfile->method('jsonSerialize')->willReturn('profile');
+        $this->scopes = [$this->scopeEntityOpenId, $this->scopeEntityProfile,];
+
+        $this->accessTokenState = [
+            'id' => self::ACCESS_TOKEN_ID,
+            'scopes' => '{"openid":"openid","profile":"profile"}',
+            'expires_at' => date('Y-m-d H:i:s', time() - 60), // expired...
+            'user_id' => 'user123',
+            'client_id' => self::CLIENT_ID,
+            'is_revoked' => false,
+            'auth_code_id' => 'authCode123',
+        ];
+
+        $this->accessTokenEntityMock = $this->createMock(AccessTokenEntity::class);
+        $this->accessTokenEntityFactoryMock = $this->createMock(AccessTokenEntityFactory::class);
+        $certFolder = dirname(__DIR__, 5) . '/docker/ssp/';
+        $privateKeyPath = $certFolder . ModuleConfig::DEFAULT_PKI_PRIVATE_KEY_FILENAME;
+        $this->privateKey = new CryptKey($privateKeyPath);
+        $this->accessTokenEntityFactory = new AccessTokenEntityFactory(
+            new Helpers(),
+            $this->privateKey,
+            $this->createMock(JsonWebTokenBuilderService::class),
+        );
     }
 
     public function useDatabase($config): void
@@ -129,7 +165,7 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
         $this->accessTokenRepository = new AccessTokenRepository(
             $moduleConfig,
             $clientRepositoryMock,
-            $this->accessTokenEntityFactoryMock,
+            $this->accessTokenEntityFactory,
         );
 
         $client = self::clientRepositoryGetClient(self::CLIENT_ID);
@@ -227,28 +263,11 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
         ];
     }
 
-    /**
-     * @return void
-     * @throws \PHPUnit\Framework\MockObject\Exception
-     */
-    public function setUp(): void
-    {
-        $this->scopeEntityOpenId = $this->createStub(ScopeEntity::class);
-        $this->scopeEntityOpenId->method('getIdentifier')->willReturn('openid');
-        $this->scopeEntityOpenId->method('jsonSerialize')->willReturn('openid');
-        $this->scopeEntityProfile = $this->createStub(ScopeEntity::class);
-        $this->scopeEntityProfile->method('getIdentifier')->willReturn('profile');
-        $this->scopeEntityProfile->method('jsonSerialize')->willReturn('profile');
-        $this->scopes = [$this->scopeEntityOpenId, $this->scopeEntityProfile,];
-
-        $this->accessTokenEntityFactoryMock = $this->createMock(AccessTokenEntityFactory::class);
-    }
-
     public static function databaseToTest(): array
     {
         return [
-            'PostgreSql' => ['pgConfig'],
-            'MySql' => ['mysqlConfig'],
+            //'PostgreSql' => ['pgConfig'],
+            //'MySql' => ['mysqlConfig'],
             'Sqlite' => ['sqliteConfig'],
         ];
     }
@@ -285,17 +304,9 @@ class RevokeTokenByAuthCodeIdTraitTest extends TestCase
     {
         $this->useDatabase(self::$$database);
 
-        $accessToken = $this->accessTokenRepository->getNewToken(
-            self::clientRepositoryGetClient(self::CLIENT_ID),
-            $this->scopes,
-            self::USER_ID,
-            self::AUTH_CODE_ID,
-            self::REQUESTED_CLAIMS,
-            self::ACCESS_TOKEN_ID,
-            (new DateTimeImmutable('yesterday', new DateTimeZone('UTC'))),
-        );
+        $this->accessTokenEntityMock->method('getState')->willReturn($this->accessTokenState);
 
-        $this->accessTokenRepository->persistNewAccessToken($accessToken);
+        $this->accessTokenRepository->persistNewAccessToken($this->accessTokenEntityMock);
 
         $isRevoked = $this->accessTokenRepository->isAccessTokenRevoked(self::ACCESS_TOKEN_ID);
         $this->assertFalse($isRevoked);
