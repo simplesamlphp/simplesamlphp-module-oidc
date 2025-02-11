f5a1eb837fe9550caafed1cd674bc8010ddebd21
Marko Ivancic <mivanci@srce.hr>
2025-02-11 12:22:35 +0100

    Update integration tests

--- tests/integration/src/Factories/FormFactoryTest.php
+++ tests/integration/src/Factories/FormFactoryTest.php
@@ -12,6 +12,7 @@ use SimpleSAML\Module\oidc\Bridges\SspBridge;
 use SimpleSAML\Module\oidc\Factories\FormFactory;
 use SimpleSAML\Module\oidc\Forms\ClientForm;
 use SimpleSAML\Module\oidc\Forms\Controls\CsrfProtection;
+use SimpleSAML\Module\oidc\Helpers;
 use SimpleSAML\Module\oidc\ModuleConfig;
 
 #[CoversClass(FormFactory::class)]
@@ -21,27 +22,32 @@ class FormFactoryTest extends TestCase
     protected MockObject $moduleConfigMock;
     protected MockObject $csrfProtectionMock;
     protected MockObject $sspBridgeMock;
+    protected MockObject $helpersMock;
 
     protected function setUp(): void
     {
         $this->moduleConfigMock = $this->createMock(ModuleConfig::class);
         $this->csrfProtectionMock = $this->createMock(CsrfProtection::class);
         $this->sspBridgeMock = $this->createMock(SspBridge::class);
+        $this->helpersMock = $this->createMock(Helpers::class);
     }
 
     protected function sut(
         ?ModuleConfig $moduleConfig = null,
         ?CsrfProtection $csrfProtection = null,
         ?SspBridge $sspBridge = null,
+        ?Helpers $helpers = null,
     ): FormFactory {
         $moduleConfig ??= $this->moduleConfigMock;
         $csrfProtection ??= $this->csrfProtectionMock;
         $sspBridge ??= $this->sspBridgeMock;
+        $helpers ??= $this->helpersMock;
 
         return new FormFactory(
             $moduleConfig,
             $csrfProtection,
             $sspBridge,
+            $helpers,
         );
     }
 
