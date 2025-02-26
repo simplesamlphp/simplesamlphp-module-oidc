80194ee0f6fc8bc23fa3031855242f2a4206df54
Marko Ivancic <mivanci@srce.hr>
2025-02-26 10:55:57 +0100

    Add unit test for redirect_uri validation

--- src/Forms/ClientForm.php
+++ src/Forms/ClientForm.php
@@ -34,6 +34,8 @@ class ClientForm extends Form
 
     /**
      * RFC3986. AppendixB. Parsing a URI Reference with a Regular Expression.
+     * From v6.*, the regex was modified to allow URI without host, to support adding entries like
+     * `openid-credential-offer://`
      */
     final public const REGEX_URI = '/^[^:]+:\/\/?([^\s\/$.?#].[^\s]*)?$/';
 
