47bc7fb35f792a3e8a1aa9bfb537d44172f93cae
Marko Ivančić <mivanci@srce.hr>
2026-08-21 16:08:11 +0200

    Fix tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,10 +6,23 @@ $projectRoot = dirname(__DIR__);
 require_once $projectRoot . '/vendor/autoload.php';
 
 // Symlink module into ssp vendor lib so that templates and urls can resolve correctly
-$linkPath = $projectRoot . '/vendor/simplesamlphp/simplesamlphp/modules/oidc';
-if (false === file_exists($linkPath)) {
-    echo "Linking '$linkPath' to '$projectRoot'\n";
-    symlink($projectRoot, $linkPath);
-} else {
-    echo "'$linkPath' linked to '$projectRoot'\n";
+$modulesDir = $projectRoot . '/vendor/simplesamlphp/simplesamlphp/modules';
+$linkPath = $modulesDir . '/oidc';
+$target = '../../../../';
+
+if (!is_dir($modulesDir)) {
+    mkdir($modulesDir, 0777, true);
+}
+
+if (is_link($linkPath)) {
+    if (!file_exists($linkPath) || (readlink($linkPath) !== $target && realpath($linkPath) !== $projectRoot)) {
+        unlink($linkPath);
+        symlink($target, $linkPath);
+        echo "Re-linking '$linkPath' to '$target'\n";
+    } else {
+        echo "'$linkPath' linked to '$projectRoot'\n";
+    }
+} elseif (!file_exists($linkPath)) {
+    echo "Linking '$linkPath' to '$target'\n";
+    symlink($target, $linkPath);
 }
